170d54758fed8e2015720cfa6f6235fa3cf50389
Supriya.patil <patilsupriya757@gmail.com>
2022-02-28 20:33:16 +0530

    first commit

---
+++ RetrievingProductdetail/src/main/java/com/simplilearn/productSearch/Databaseconnect.java
@@ -0,0 +1,31 @@
+package com.simplilearn.productSearch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+public class Databaseconnect 
+{
+	public static Connection getconnect(Properties prop)
+	{
+		Connection connection=null;
+		try 
+		{
+			String driver=prop.getProperty("driver");
+			String url=prop.getProperty("url");
+			String username=prop.getProperty("username");
+			String password=prop.getProperty("password");
+			
+			// load driver
+			Class.forName(driver);
+			connection=DriverManager.getConnection(url, username, password);
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error Occured :"+e);
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
+
